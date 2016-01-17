b7ba2aae6d3936e9e09ea277636dad995036c5ef
Sean Werkema <sean@werkema.com>
2016-01-17 02:09:11 -0500

    Unifying the build scripts (a little bit) for Darwin vs. Linux.

--- SmileC/scripts/getsrc.sh
+++ SmileC/scripts/getsrc.sh
@@ -12,20 +12,20 @@ esac
 shift
 done
 
+PLATFORM=`uname -s`
+if [ $PLATFORM == Darwin ]
+then
+	SED='sed -E'
+else
+	SED='sed -r'
+fi
+
 printf '# This file is generated.  Do not edit!'
 
 printf '\nSRCS = \\\n'
 
-PLATFORM=`uname -s`
-if [ $PLATFORM == Darwin ]
-then
-	grep '<ClCompile Include' $VCXPROJ \
-		| sed -E -e 's/^[[:space:]]*<ClCompile Include=\"//; s/\"[[:space:]]*\/*>[[:space:]]*$//; s/\\/\//g; s/^(.*)$/    \1 \\/; $s/\\//;' \
-		| sort
-else
-	grep '<ClCompile Include' $VCXPROJ \
-		| sed -e 's/^\s*<ClCompile Include=\"//; s/\"\s*\/\?>\s*$//; s/\\/\//g; s/^\(.*\)$/\t\1 \\/; $s/\\//;' \
-		| sort
-fi
+grep '<ClCompile Include' $VCXPROJ \
+	| $SED -e 's/^[[:space:]]*<ClCompile Include=\"//; s/\"[[:space:]]*\/*>[[:space:]]*$//; s/\\/\//g; s/^(.*)$/    \1 \\/; $s/\\//;' \
+	| sort
 printf '\n# End of generated file.\n'
 
