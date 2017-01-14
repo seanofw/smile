6c5f7ffbcee19934b03651e9bf357477d00ad9a3
seanofw <sean@werkema.com>
2017-01-14 09:44:49 -0500

    Bump copyright dates to 2017.

--- SmileC/smilerunner/main.c
+++ SmileC/smilerunner/main.c
@@ -1,6 +1,6 @@
 //---------------------------------------------------------------------------------------
 //  Smile Programming Language Interpreter (Command-Line Runner)
-//  Copyright 2004-2016 Sean Werkema
+//  Copyright 2004-2017 Sean Werkema
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
