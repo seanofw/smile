4239338c440fd1b1c144beda4a2c8ed11a49b155
seanofw <sean@werkema.com>
2017-02-19 12:03:37 -0500

    Refactor [$while] compiling to use cleaner, smaller functions.

--- SmileC/smilelib/src/eval/compiler/compile_while.c
+++ SmileC/smilelib/src/eval/compiler/compile_while.c
@@ -24,18 +24,17 @@
 #include <smile/parsing/internal/parsedecl.h>
 #include <smile/parsing/internal/parsescope.h>
 
+static void CompileWhileWithPreAndPost(Compiler compiler, Bool not, SmileObject condition, SmileObject preClause, SmileObject postClause);
+static void CompileWhileWithPre(Compiler compiler, Bool not, SmileObject condition, SmileObject preClause);
+static void CompileWhileWithPost(Compiler compiler, Bool not, SmileObject condition, SmileObject postClause);
+static void CompileWhileWithNoBody(Compiler compiler, Bool not, SmileObject condition);
+
 // Form: [$while pre-body cond post-body]
 void Compiler_CompileWhile(Compiler compiler, SmileList args)
 {
 	SmileObject condition, preClause, postClause;
 	Int postKind, tailKind;
 	Bool not, hasPre, hasPost;
-	ByteCodeSegment segment;
-	Int bDelta, jmpDelta;
-	Int b, jmp, bLabel, jmpLabel;
-	Int offset;
-
-	segment = compiler->currentFunction->byteCodeSegment;
 
 	// Must be an expression of the form [$while cond postBody] or [$while pre-body cond post-body].
 	if (SMILE_KIND(args) != SMILE_KIND_LIST || SMILE_KIND(args->d) != SMILE_KIND_LIST) {
@@ -84,135 +83,201 @@ void Compiler_CompileWhile(Compiler compiler, SmileList args)
 	hasPre = (SMILE_KIND(preClause) != SMILE_KIND_NULL);
 	hasPost = (SMILE_KIND(postClause) != SMILE_KIND_NULL);
 
+	// Dispatch to an optimized compile depending on which flavor of [$while] this is.
 	if (hasPre && hasPost) {
-		// Form: do {...} while cond then {...}
-		//
-		// Emit this in order of:
-		//   l1: eval stmts1
-		//       eval cond
-		//       branch l2
-		//		pop1
-		//       eval stmts2
-		//		pop1
-		//       jmp l1
-		//   l2:
-
-		jmpLabel = EMIT0(Op_Label, 0);
-
-		Compiler_CompileExpr(compiler, preClause);
-
-		Compiler_CompileExpr(compiler, condition);
-
-		b = EMIT0(not ? Op_Bf : Op_Bt, -1);
-
-		Compiler_EmitPop1(compiler);
-
-		Compiler_CompileExpr(compiler, postClause);
-		Compiler_EmitPop1(compiler);
-
-		jmp = EMIT0(Op_Jmp, 0);
-
-		bLabel = EMIT0(Op_Label, 0);
-
-		bDelta = bLabel - b;
-		FIX_BRANCH(b, bDelta);
-		FIX_BRANCH(bLabel, -bDelta);
-
-		jmpDelta = jmpLabel - jmp;
-		FIX_BRANCH(jmp, jmpDelta);
-		FIX_BRANCH(jmpLabel, -jmpDelta);
-
-		// By the time we reach this point, one iteration will be left on the stack.
-		compiler->currentFunction->currentStackDepth++;
+		CompileWhileWithPreAndPost(compiler, not, condition, preClause, postClause);
 	}
 	else if (hasPre) {
-		// Form: do {...} while cond
-		//
-		// Emit this in order of:
-		//       jmp l1
-		//   l2: pop1
-		//   l1: eval stmts
-		//       eval cond
-		//       branch l2
-
-		jmp = EMIT0(Op_Jmp, 0);
-
-		bLabel = EMIT0(Op_Label, 0);
-
-		Compiler_EmitPop1(compiler);
-
-		jmpLabel = EMIT0(Op_Label, 0);
-
-		Compiler_CompileExpr(compiler, preClause);
-
-		Compiler_CompileExpr(compiler, condition);
-
-		b = EMIT0(not ? Op_Bf : Op_Bt, -1);
-
-		bDelta = bLabel - b;
-		FIX_BRANCH(b, bDelta);
-		FIX_BRANCH(bLabel, -bDelta);
-
-		jmpDelta = jmpLabel - jmp;
-		FIX_BRANCH(jmp, jmpDelta);
-		FIX_BRANCH(jmpLabel, -jmpDelta);
-
-		// By the time we reach this point, one iteration will be left on the stack.
-		compiler->currentFunction->currentStackDepth++;
+		CompileWhileWithPre(compiler, not, condition, preClause);
 	}
 	else if (hasPost) {
-		// Form: while cond do {...}
-		//
-		// Emit this in order of:
-		//       ldnull
-		//       jmp l1
-		//   l2: pop1
-		//       eval stmts
-		//   l1: eval cond
-		//       branch l2
-
-		EMIT0(Op_LdNull, +1);
-
-		jmp = EMIT0(Op_Jmp, 0);
-
-		bLabel = EMIT0(Op_Label, 0);
-
-		Compiler_EmitPop1(compiler);
-
-		Compiler_CompileExpr(compiler, postClause);
-
-		jmpLabel = EMIT0(Op_Label, 0);
-
-		Compiler_CompileExpr(compiler, condition);
-
-		b = EMIT0(not ? Op_Bf : Op_Bt, -1);
-
-		bDelta = bLabel - b;
-		FIX_BRANCH(b, bDelta);
-		FIX_BRANCH(bLabel, -bDelta);
-
-		jmpDelta = jmpLabel - jmp;
-		FIX_BRANCH(jmp, jmpDelta);
-		FIX_BRANCH(jmpLabel, -jmpDelta);
+		CompileWhileWithPost(compiler, not, condition, postClause);
 	}
 	else {
-		// Form: while cond {}
-		//
-		// Emit this in order of:
-		//   l1: eval cond
-		//       branch l1
-		//       ldnull
-
-		bLabel = EMIT0(Op_Label, 0);
-
-		Compiler_CompileExpr(compiler, condition);
-
-		b = EMIT0(not ? Op_Bf : Op_Bt, -1);
-
-		EMIT0(Op_LdNull, +1);
-
-		bDelta = bLabel - b;
-		FIX_BRANCH(b, bDelta);
-		FIX_BRANCH(bLabel, -bDelta);
+		CompileWhileWithNoBody(compiler, not, condition);
 	}
+}
+
+// Form: do {...} while cond then {...}
+static void CompileWhileWithPreAndPost(Compiler compiler, Bool not, SmileObject condition, SmileObject preClause, SmileObject postClause)
+{
+	ByteCodeSegment segment;
+	Int bDelta, jmpDelta;
+	Int b, jmp, bLabel, jmpLabel;
+	Int offset;
+
+	segment = compiler->currentFunction->byteCodeSegment;
+
+	// Emit this in order of:
+	//
+	//   l1: eval preClause
+	//
+	//       eval cond
+	//       branch l2
+	//
+	//       pop1		// pop the last preClause
+	//
+	//       eval postClause
+	//       pop1
+	//
+	//       jmp l1
+	//
+	//   l2:	// left with the last preClause on the stack.
+
+	jmpLabel = EMIT0(Op_Label, 0);
+
+	Compiler_CompileExpr(compiler, preClause);
+
+	Compiler_CompileExpr(compiler, condition);
+
+	b = EMIT0(not ? Op_Bf : Op_Bt, -1);
+
+	Compiler_EmitPop1(compiler);
+
+	Compiler_CompileExpr(compiler, postClause);
+	Compiler_EmitPop1(compiler);
+
+	jmp = EMIT0(Op_Jmp, 0);
+
+	bLabel = EMIT0(Op_Label, 0);
+
+	bDelta = bLabel - b;
+	FIX_BRANCH(b, bDelta);
+	FIX_BRANCH(bLabel, -bDelta);
+
+	jmpDelta = jmpLabel - jmp;
+	FIX_BRANCH(jmp, jmpDelta);
+	FIX_BRANCH(jmpLabel, -jmpDelta);
+
+	// By the time we reach this point, one iteration will be left on the stack.
+	compiler->currentFunction->currentStackDepth++;
+}
+
+// Form: do {...} while cond
+static void CompileWhileWithPre(Compiler compiler, Bool not, SmileObject condition, SmileObject preClause)
+{
+	ByteCodeSegment segment;
+	Int bDelta, jmpDelta;
+	Int b, jmp, bLabel, jmpLabel;
+	Int offset;
+
+	segment = compiler->currentFunction->byteCodeSegment;
+
+	// Emit this in order of:
+	//
+	//       jmp l1
+	//
+	//   l2: pop1	// pop last preClause from stack
+	//
+	//   l1: eval preClause
+	//
+	//       eval cond
+	//       branch l2
+	//
+	//   // stack is left with last preClause sitting on it.
+
+	jmp = EMIT0(Op_Jmp, 0);
+
+	bLabel = EMIT0(Op_Label, 0);
+
+	Compiler_EmitPop1(compiler);
+
+	jmpLabel = EMIT0(Op_Label, 0);
+
+	Compiler_CompileExpr(compiler, preClause);
+
+	Compiler_CompileExpr(compiler, condition);
+
+	b = EMIT0(not ? Op_Bf : Op_Bt, -1);
+
+	bDelta = bLabel - b;
+	FIX_BRANCH(b, bDelta);
+	FIX_BRANCH(bLabel, -bDelta);
+
+	jmpDelta = jmpLabel - jmp;
+	FIX_BRANCH(jmp, jmpDelta);
+	FIX_BRANCH(jmpLabel, -jmpDelta);
+
+	// By the time we reach this point, one iteration will be left on the stack.
+	compiler->currentFunction->currentStackDepth++;
+}
+
+// Form: while cond do {...}
+static void CompileWhileWithPost(Compiler compiler, Bool not, SmileObject condition, SmileObject postClause)
+{
+	ByteCodeSegment segment;
+	Int bDelta, jmpDelta;
+	Int b, jmp, bLabel, jmpLabel;
+	Int offset;
+
+	segment = compiler->currentFunction->byteCodeSegment;
+
+	// Emit this in order of:
+	//
+	//       ldnull	// Initial result, if the initial condition is false.
+	//       jmp l1
+	//
+	//   l2: pop1	// pop last postClause (or initial null).
+	//       eval postClause
+	//
+	//   l1: eval cond
+	//       branch l2
+	//
+	//   // stack is left with either the initial null or the last postClause.
+
+	EMIT0(Op_LdNull, +1);
+
+	jmp = EMIT0(Op_Jmp, 0);
+
+	bLabel = EMIT0(Op_Label, 0);
+
+	Compiler_EmitPop1(compiler);
+
+	Compiler_CompileExpr(compiler, postClause);
+
+	jmpLabel = EMIT0(Op_Label, 0);
+
+	Compiler_CompileExpr(compiler, condition);
+
+	b = EMIT0(not ? Op_Bf : Op_Bt, -1);
+
+	bDelta = bLabel - b;
+	FIX_BRANCH(b, bDelta);
+	FIX_BRANCH(bLabel, -bDelta);
+
+	jmpDelta = jmpLabel - jmp;
+	FIX_BRANCH(jmp, jmpDelta);
+	FIX_BRANCH(jmpLabel, -jmpDelta);
+}
+
+// Form: while cond {}
+static void CompileWhileWithNoBody(Compiler compiler, Bool not, SmileObject condition)
+{
+	ByteCodeSegment segment;
+	Int bDelta;
+	Int b, bLabel;
+	Int offset;
+
+	segment = compiler->currentFunction->byteCodeSegment;
+
+	// Emit this in order of:
+	//
+	//   l1: eval cond
+	//       branch l1
+	//
+	//       ldnull
+	//
+	//   // stack is left with a null on it, since there is no body.
+
+	bLabel = EMIT0(Op_Label, 0);
+
+	Compiler_CompileExpr(compiler, condition);
+
+	b = EMIT0(not ? Op_Bf : Op_Bt, -1);
+
+	EMIT0(Op_LdNull, +1);
+
+	bDelta = bLabel - b;
+	FIX_BRANCH(b, bDelta);
+	FIX_BRANCH(bLabel, -bDelta);
 }
