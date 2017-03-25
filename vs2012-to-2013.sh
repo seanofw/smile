340f37064303b0041fa409d9c33708156cf22836
seanofw <sean@werkema.com>
2017-03-25 12:48:36 -0400

    Move everything in SmileC/ to root. Clean up VS2012 cruft.

--- vs2012-to-2013.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pushd SmileC/smilelib; ./vs2012-to-2013.sh; popd
-pushd SmileC/smilelibtests; ./vs2012-to-2013.sh; popd
-
--- vs2013-to-2012.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pushd SmileC/smilelib; ./vs2013-to-2012.sh; popd
-pushd SmileC/smilelibtests; ./vs2013-to-2012.sh; popd
-
