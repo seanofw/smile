0ded64e54d57ee7a94582e874a0a6f9de7d73a74
seanofw <sean@werkema.com>
2018-04-15 10:31:41 -0400

    Enhance stack traces on Unix for a crash.

--- smilelibtests/main.c
+++ smilelibtests/main.c
@@ -89,8 +89,89 @@ static Bool ParseCommandLine(int argc, const char **argv)
 	return True;
 }
 
+#if ((SMILE_OS & SMILE_OS_FAMILY) == SMILE_OS_UNIX_FAMILY) && (SMILE_COMPILER == SMILE_COMPILER_GCC)
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#ifndef __USE_GNU
+#define __USE_GNU
+#endif
+
+#include <execinfo.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
+#include <unistd.h>
+
+/* This structure mirrors the one found in /usr/include/asm/ucontext.h */
+typedef struct _sig_ucontext {
+	unsigned long     uc_flags;
+	struct ucontext   *uc_link;
+	stack_t           uc_stack;
+	struct sigcontext uc_mcontext;
+	sigset_t          uc_sigmask;
+} sig_ucontext_t;
+
+void SegFaultHandler(int sig_num, siginfo_t * info, void * ucontext)
+{
+	void *             array[50];
+	void *             caller_address;
+	char **            messages;
+	int                size, i;
+	sig_ucontext_t *   uc;
+
+	uc = (sig_ucontext_t *)ucontext;
+
+	/* Get the address at the time the signal was raised */
+#if defined(__i386__) // gcc specific
+	caller_address = (void *)uc->uc_mcontext.eip; // EIP: x86 specific
+#elif defined(__x86_64__) // gcc specific
+	caller_address = (void *)uc->uc_mcontext.rip; // RIP: x86_64 specific
+#else
+#error Unsupported architecture. // TODO: Add support for other arch.
+#endif
+
+	fprintf(stderr, "signal %d (%s), address is %p from %p\n",
+		sig_num, strsignal(sig_num), info->si_addr,
+		(void *)caller_address);
+
+	size = backtrace(array, 50);
+
+	/* overwrite sigaction with caller's address */
+	array[1] = caller_address;
+
+	messages = backtrace_symbols(array, size);
+
+	/* skip first stack frame (points here) */
+	for (i = 1; i < size && messages != NULL; ++i) {
+		fprintf(stderr, "at (%d): %s\n", i, messages[i]);
+	}
+
+	free(messages);
+
+	exit(1);
+}
+
+#endif
+
 int main(int argc, const char **argv)
 {
+#	if ((SMILE_OS & SMILE_OS_FAMILY) == SMILE_OS_UNIX_FAMILY) && (SMILE_COMPILER == SMILE_COMPILER_GCC)
+	{
+		struct sigaction sigact;
+
+		sigact.sa_sigaction = SegFaultHandler;
+		sigact.sa_flags = SA_RESTART | SA_SIGINFO;
+
+		if (sigaction(SIGSEGV, &sigact, (struct sigaction *)NULL) != 0) {
+			fprintf(stderr, "Unable to change signal handler for SEGV: (%s)\n", strsignal(SIGSEGV));
+			exit(-2);
+		}
+	}
+#	endif
+
 	if (!ParseCommandLine(argc, argv))
 		return -1;
 
