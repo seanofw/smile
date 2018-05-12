00151217cb265e0a3fd0d6ad34f73d8e175c9e69
seanofw <sean@werkema.com>
2018-05-11 22:06:54 -0400

    Add SymbolMap.each, and rename 'key?' to be 'contains?' instead.

--- smilelib/src/smiletypes/collections/smilesymbolmap_base.c
+++ smilelib/src/smiletypes/collections/smilesymbolmap_base.c
@@ -46,6 +46,11 @@ static Byte _symbolMapMemberChecks[] = {
 	0, 0,
 };
 
+static Byte _eachChecks[] = {
+	SMILE_KIND_MASK, SMILE_KIND_SYMBOLMAP,
+	SMILE_KIND_MASK, SMILE_KIND_FUNCTION,
+};
+
 //-------------------------------------------------------------------------------------------------
 // Generic type conversion
 
@@ -155,7 +160,7 @@ SMILE_EXTERNAL_FUNCTION(Remove)
 	return SmileUnboxedBool_From(result);
 }
 
-SMILE_EXTERNAL_FUNCTION(Key)
+SMILE_EXTERNAL_FUNCTION(ContainsKey)
 {
 	SmileSymbolMap symbolMap = (SmileSymbolMap)argv[0].obj;
 	Symbol symbol = argv[1].unboxed.symbol;
@@ -201,6 +206,129 @@ SMILE_EXTERNAL_FUNCTION(Pairs)
 
 //-------------------------------------------------------------------------------------------------
 
+typedef struct EachInfoStruct {
+	SmileSymbolMap symbolMap;
+	SmileFunction function;
+	Int32 bucket, nodeIndex;
+	Int32 index, numArgs;
+} *EachInfo;
+
+static Int EachCommon(ClosureStateMachine closure)
+{
+	EachInfo eachInfo = (EachInfo)closure->state;
+	struct Int32DictInt *dict = &eachInfo->symbolMap->dict._opaque;
+	struct Int32DictNode *node;
+	Int32 index;
+
+	// Get the current list node, and then quietly move the index to the next list node.
+	node = &dict->heap[eachInfo->nodeIndex];
+	eachInfo->nodeIndex = node->next;
+	index = eachInfo->index++;
+
+	// Body: Set up to call the user's function with the next map item (a constructed pair).
+	Closure_PushBoxed(closure, eachInfo->function);
+
+	// Push the arguments to the user function.
+	switch (eachInfo->numArgs) {
+		case 1:
+			// One arg: Pass a list, shaped like |[key value]|.
+			Closure_PushBoxed(closure, SmileList_CreateTwo(
+				SmileSymbol_Create((Symbol)node->key),
+				(SmileObject)node->value)
+			);
+			return 1;
+
+		case 2:
+			// Two args: Pass a list and index, shaped like |[key value] index|.
+			Closure_PushBoxed(closure, SmileList_CreateTwo(
+				SmileSymbol_Create((Symbol)node->key),
+				(SmileObject)node->value)
+			);
+			Closure_PushUnboxedInt64(closure, index);
+			return 2;
+
+		default:
+			// Three args: Pass all the pieces individually, shaped like |key value index|
+			Closure_PushUnboxedSymbol(closure, node->key);
+			Closure_UnboxAndPush(closure, (SmileObject)node->value);
+			Closure_PushUnboxedInt64(closure, index);
+			return 3;
+	}
+}
+
+static Int EachStartBucket(ClosureStateMachine closure)
+{
+	EachInfo eachInfo = (EachInfo)closure->state;
+	struct Int32DictInt *dict = &eachInfo->symbolMap->dict._opaque;
+
+	//---------- begin outer for-loop iteration ----------
+
+nextBucket:
+	// Condition: If we've run out of buckets, we're done.
+	if (eachInfo->bucket > dict->mask) {
+		Closure_Pop(closure);	// Pop the previous return value
+		Closure_PushBoxed(closure, eachInfo->symbolMap);	// and push 'symbolMap' as the new return value.
+		return -1;
+	}
+
+	// Start the inner loop that walks the linked list of nodes for this bucket.
+	eachInfo->nodeIndex = dict->buckets[eachInfo->bucket];
+
+	// If the linked list is empty, move to the next bucket.
+	if (eachInfo->nodeIndex < 0) {
+		eachInfo->bucket++;
+		goto nextBucket;
+	}
+
+	//---------- begin inner for-loop iteration ----------
+
+	return EachCommon(closure);
+}
+
+static Int EachNext(ClosureStateMachine closure)
+{
+	EachInfo eachInfo = (EachInfo)closure->state;
+
+	// If the current linked list is empty, restart at the next bucket.
+	if (eachInfo->nodeIndex < 0) {
+		eachInfo->bucket++;
+		return EachStartBucket(closure);
+	}
+
+	//---------- end previous inner for-loop iteration ----------
+
+	//---------- begin next inner for-loop iteration ----------
+
+	return EachCommon(closure);
+}
+
+SMILE_EXTERNAL_FUNCTION(Each)
+{
+	// We use Eval's state-machine construct to avoid recursing deeper on the C stack.
+	SmileSymbolMap symbolMap = (SmileSymbolMap)argv[0].obj;
+	SmileFunction function = (SmileFunction)argv[1].obj;
+	Int minArgs, maxArgs;
+	EachInfo eachInfo;
+	ClosureStateMachine closure;
+
+	SmileFunction_GetArgCounts(function, &minArgs, &maxArgs);
+
+	closure = Eval_BeginStateMachine(EachStartBucket, EachNext);
+
+	eachInfo = (EachInfo)closure->state;
+	eachInfo->symbolMap = symbolMap;
+	eachInfo->function = function;
+	eachInfo->bucket = 0;
+	eachInfo->index = 0;
+	eachInfo->numArgs = maxArgs < 3 ? maxArgs : 3;
+
+	Closure_PushBoxed(closure, NullObject);	// Initial "return" value from 'each'.
+
+	return (SmileArg) { NULL };	// We have to return something, but this value will be ignored.
+}
+
+//-------------------------------------------------------------------------------------------------
+
 void SmileSymbolMap_Setup(SmileUserObject base)
 {
 	SetupFunction("bool", ToBool, NULL, "map", ARG_CHECK_EXACT, 1, 1, 0, NULL);
@@ -216,9 +344,8 @@ void SmileSymbolMap_Setup(SmileUserObject base)
 	SetupFunction("add", Add, NULL, "map symbol value", ARG_CHECK_EXACT | ARG_CHECK_TYPES, 3, 3, 3, _symbolMapMemberChecks);
 	SetupFunction("remove", Remove, NULL, "map symbols...", ARG_CHECK_MIN, 2, 0, 0, NULL);
 
-	SetupFunction("key?", Key, NULL, "map symbol", ARG_CHECK_EXACT | ARG_CHECK_TYPES, 2, 2, 2, _symbolMapMemberChecks);
-	SetupSynonym("key?", "has-key?");
-	SetupSynonym("key?", "contains-key?");
+	SetupFunction("contains-key?", ContainsKey, NULL, "map symbol", ARG_CHECK_EXACT | ARG_CHECK_TYPES, 2, 2, 2, _symbolMapMemberChecks);
+	SetupSynonym("contains-key?", "contains?");
 
 	SetupFunction("keys", Keys, NULL, "map", ARG_CHECK_EXACT | ARG_CHECK_TYPES, 1, 1, 1, _symbolMapChecks);
 	SetupSynonym("keys", "keys-of");
@@ -226,4 +353,7 @@ void SmileSymbolMap_Setup(SmileUserObject base)
 	SetupSynonym("values", "values-of");
 	SetupFunction("pairs", Pairs, NULL, "map", ARG_CHECK_EXACT | ARG_CHECK_TYPES, 1, 1, 1, _symbolMapChecks);
 	SetupSynonym("pairs", "pairs-of");
+	SetupSynonym("pairs", "list");
+
+	SetupFunction("each", Each, NULL, "map fn", ARG_CHECK_EXACT | ARG_CHECK_TYPES | ARG_STATE_MACHINE, 2, 2, 2, _eachChecks);
 }
