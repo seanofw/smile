e5624e0f78c7bea027ad058d84911ccb996f82e1
seanofw <sean@werkema.com>
2018-07-19 09:47:36 -0400

    Add beginnings of bootstrap.

---
+++ bootstrap-precompiler/stdafx.h
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------------------
+//  Smile Programming Language Interpreter (Command-Line Runner)
+//  Copyright 2004-2017 Sean Werkema
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//---------------------------------------------------------------------------------------
+
+#ifndef __STDAFX_H__
+#define __STDAFX_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include <smile.h>
+#include <smile/version.h>
+#include <smile/smiletypes/smilelist.h>
+#include <smile/smiletypes/text/smilesymbol.h>
+#include <smile/parsing/lexer.h>
+#include <smile/parsing/parser.h>
+#include <smile/eval/eval.h>
+
+#endif
